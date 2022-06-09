0cbb26d925afbbf87ed5f3f64c2283ebaeea7f98
noobdev47 <sumairsaifofficial@gmail.com>
2022-06-09 12:50:57 +0500

    list items now have more info button and better borders

--- src/components/list_item/ListItem.jsx
+++ src/components/list_item/ListItem.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const ListItem = ({ data }) => {
 	return (
-		<div className='p-2 m-2 flex flex-col basis-2/3 md:basis-5/12 lg:basis-5/12 sm:basis-2/3 border border-slate-300 rounded'>
-			<img src={data.image} className='border-b-2' />
-			<p className='m-0 font-sans text-center text-base mt-2 text-slate-400'>
+		<div className='p-2 m-2 flex flex-col basis-2/3 sm:basis-5/12 md:basis-5/12 lg:basis-5/12  border border-slate-300 rounded'>
+			<img src={data.image} className='border-b' />
+			<p className='m-0 font-sans text-center text-xl my-2 text-slate-400'>
 				SKU-{data.sku}
 			</p>
-			<div className='flex flex-row items-stretch'>
+			<div className='flex flex-row items-stretch pb-2 border-b'>
 				<div className='flex flex-col flex-1 items-center justify-evenly'>
 					<div className='flex flex-col items-center'>
 						<p className='m-0 font-sans text-base font-light'>{data.type}</p>
@@ -41,6 +41,12 @@ const ListItem = ({ data }) => {
 					</div>
 				</div>
 			</div>
+			<p className='font-sans py-2 text-2xl text-sky-500 text-center'>
+				{data.price}
+			</p>
+			<button className='py-3 hover:shadow-lg hover:shadow-sky-500/50 transition ease-in-out duration-300 font-medium text-lg rounded font-sans border border-sky-500 bg-white text-sky-500 hover:bg-sky-500 hover:text-white'>
+				MORE INFO
+			</button>
 		</div>
 	)
 }
