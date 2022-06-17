829d0dc340889a12e7319c525b4cf894eaab7286
noobdev47 <sumairsaifofficial@gmail.com>
2022-06-17 18:27:56 +0500

    responsiveness changes

--- src/components/stone_reservation/StoneReservation.jsx
+++ src/components/stone_reservation/StoneReservation.jsx
@@ -7,44 +7,44 @@ const StoneReservation = ({ Stone }) => {
 
 	if (Stone !== undefined)
 		return Object.keys(Stone.stoneDetail).length !== 0 ? (
-			<div className='flex flex-row border-b border-slate-300 justify-between pb-4'>
-				<div className='flex flex-col justify-center items-center'>
+			<div className='flex lg:flex-row md:flex-row sm:flex-col flex-col border-b border-slate-300 justify-between pb-4'>
+				<div className='flex lg:flex-col md:flex-col sm:flex-row flex-row justify-center items-center'>
 					<img
 						width='100'
 						height='100'
 						src={Stone.stoneDetail.image}
-						className='border border-slate-300 mb-2 p-2 rounded'
+						className='border border-slate-300 px-0.5 lg:mb-2 lg:mr-0 md:mb-2 md:mr-0 sm:mb-0 sm:mr-2 mb-0 mr-2 rounded hover:border-slate-500 cursor-pointer transition ease-in-out duration-300'
 					/>
 					<img
 						width='100'
 						height='100'
-						className='border border-slate-300 p-2 rounded'
 						src='https://gemsource.riogrande.com/public/images/site/360-transparent.png'
+						className='border border-slate-300 px-0.5 py-5 rounded hover:border-slate-500 cursor-pointer transition ease-in-out duration-300'
 					/>
 				</div>
-				<div className='flex flex-col'>
+				<div className='flex flex-col sm:items-center items-center'>
 					<img src={Stone.stoneDetail.image} width='300' height='300' />
 					<p className='text-xs text-center'>Actual gemstone image</p>
 				</div>
 				<div className='flex flex-col justify-center items-stretch'>
-					<div className='flex flex-row text-2xl font-normal mb-1'>
+					<div className='flex flex-row text-2xl font-normal mb-1 justify-center'>
 						<p className='mr-1'>{Stone.stoneDetail.carat} Carat</p>
 						<p className='mr-1'>{Stone.stoneDetail.shape}</p>
 						<p>{Stone.stoneDetail.type}</p>
 					</div>
-					<div className='flex flex-row text-base font-normal mb-1'>
+					<div className='flex flex-row text-base font-normal mb-1 justify-center'>
 						<p className='mr-1'>Color: Blue |</p>
 						<p className='mr-1'>Clarity: {Stone.stoneDetail.clarity} |</p>
 						<p>Treatment: Heated</p>
 					</div>
-					<div className='flex flex-row text-sm font-normal mb-16'>
+					<div className='flex flex-row text-sm font-normal mb-16 justify-center'>
 						<p className='mr-1'>ICL</p>
 						<p className='underline'>View Report {'>>'}</p>
 					</div>
-					<div className='flex flex-row text-2xl font-normal mb-1'>
+					<div className='flex flex-row text-2xl font-normal mb-1 justify-center'>
 						<p>{Stone.stoneDetail.price}</p>
 					</div>
-					<div className='flex flex-row text-base font-normal mb-1'>
+					<div className='flex flex-row text-base font-normal mb-1 justify-center'>
 						<p>Price is for the Zircon only.</p>
 					</div>
 					<div className='flex flex-row'>
